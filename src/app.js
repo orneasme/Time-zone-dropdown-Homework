fb2dba53f0dcebdbc72cbc8a54bf069b7b5241cc
orneasme <123243041+orneasme@users.noreply.github.com>
2023-02-01 14:29:03 +0100

    added js time alert for Bs as and Zürich

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
 function showCityTime(event) {
+  let formattedDate = "H:mm";
   if (event.target.value === "Sydney") {
-    alert(moment().tz("Australia/Sydney").format("dddd, MMMM D, YYYY h:m A"));
+    alert(
+      moment().tz("Australia/Sydney").format(`it is ${formattedDate} in Sydney`)
+    );
+  }
+  if (event.target.value === "Zürich") {
+    alert(moment().tz("Europe/Zurich").format("dddd, MMMM D, YYYY H:mm "));
+  }
+  if (event.target.value === "Buenos Aires") {
+    alert(
+      moment().tz("America/Buenos_Aires").format("dddd, MMMM D, YYYY H:mm ")
+    );
   }
 }
 
